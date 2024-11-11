3e8a85a03cc4e1c9bd44ffb31841cb0b7993264e
jianbot007 <90086537+jianbot007@users.noreply.github.com>
2024-11-11 16:30:45 +0600

    Task

--- Lab3/task7.php
+++ Lab3/task7.php
@@ -9,4 +9,26 @@ for( $i = 0 ; $i < 3 ; $i++){
 }
 
 
+for( $i = 2 ; $i >= 0 ; $i--){
+    echo "<br>";
+    $num =1;
+   for($j = $i ; $j >= 0; $j--){
+    echo "$num";
+    $num++;
+}
+}
+
+$Sign = 'A';
+
+for( $i = 0 ; $i < 3 ; $i++){
+    echo "<br>";
+   for($j = $i ; $j >= 0; $j--){
+    echo "$Sign";
+    $Sign++;
+}
+}
+
+
+
+
 ?>
